085308e199cd7d41e061c08b11a8792b22a04057
DreamingLri <3507415455@qq.com>
2024-07-31 14:01:43 +0800

    :sparkles: add commands

--- mirror_world_updater/command/command.py
+++ mirror_world_updater/command/command.py
@@ -22,8 +22,11 @@ class CommandManager:
     @classmethod
     def help_info(cls, source: CommandSource, what: Optional[str]):
         reply_message(source, tr('help.help_header'))
-        if what is not None and what not in COMMAND_HELP_LIST:
-            reply_message(source, tr('help.no_help', RText(mkcmd(what), RColor.gray)))
+        if what is None:
+            reply_message(source, tr('help.help', RText(what, RColor.red)))
+            reply_message(source, tr('help.sync', RText(what, RColor.red)))
+        else:
+            reply_message(source, tr('help.' + what, RText(what, RColor.red)))
 
     def register_commands(self):
         builder = SimpleCommandBuilder()
