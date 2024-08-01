0eb0e837269a5d57d668432a1d9ebd1bfe148de0
DreamingLri <3507415455@qq.com>
2024-08-01 14:43:47 +0800

    ✨ add sync_world task

--- mirror_world_updater/mcdr/task/sync_world_task.py
+++ mirror_world_updater/mcdr/task/sync_world_task.py
@@ -1,1 +1,25 @@
+from abc import ABC
+from typing import Union
+
 from mcdreforged.api.all import *
+
+from mirror_world_updater.mcdr.task.basic_task import _BasicTask
+
+
+class SyncWorldTask(_BasicTask[None], ABC):
+    def __init__(self, source: CommandSource):
+        super().__init__(source)
+
+    @property
+    def id(self):
+        return 'sync'
+
+    def reply(self, msg: Union[str, RTextBase], *, with_prefix: bool = False):
+        super().reply(msg, with_prefix=with_prefix)
+
+    def run(self) -> None:
+        pass
+
+
+
+
