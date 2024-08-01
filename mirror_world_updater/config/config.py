f1ce8aaae64318f24106e91fd79f0da5ee23b065
DreamingLri <3507415455@qq.com>
2024-08-01 14:42:56 +0800

    💄 add upstream config

--- mirror_world_updater/config/config.py
+++ mirror_world_updater/config/config.py
@@ -1,14 +1,17 @@
 import functools
 
 from mcdreforged.api.all import Serializable
-from mirror_world_updater.config.command_config import CommandConfig
 from typing import Optional
+
+from mirror_world_updater.config.command_config import CommandConfig
+from mirror_world_updater.config.path_config import Paths
 
 
 class Config(Serializable):
     enabled: bool = True
     backup_before_update: bool = True
     command: CommandConfig = CommandConfig()
+    paths: Paths = Paths()
 
     @classmethod
     def get(cls) -> 'Config':
