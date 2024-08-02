5bf4ed85a834bb5787c3717b201c29088cd347bf
DreamingLri <3507415455@qq.com>
2024-08-02 10:33:17 +0800

    ✨ add items func and fix permission bug

--- mirror_world_updater/config/command_config.py
+++ mirror_world_updater/config/command_config.py
@@ -3,14 +3,17 @@ from mirror_world_updater import constants
 
 
 class CommandPermissions(Serializable):
-    root: 0
-    help: 1
-    sync: 3
+    root: int = 0
+    help: int = 1
+    sync: int = 3
 
     def get(self, literal: str) -> int:
         if literal.startswith('_'):
             raise KeyError(literal)
         return getattr(self, literal, constants.DEFAULT_COMMAND_PERMISSION_LEVEL)
+
+    def items(self):
+        return self.serialize().items()
 
 
 class CommandConfig(Serializable):
