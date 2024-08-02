b3bdf169c8f255bd5697ba8257395f4c5fcf0a54
DreamingLri <3507415455@qq.com>
2024-08-02 16:42:49 +0800

    ✨ add command format

--- mirror_world_updater/mcdr/text_components.py
+++ mirror_world_updater/mcdr/text_components.py
@@ -1,6 +1,8 @@
 from typing import Any
 from mcdreforged.api.all import *
 
+from mirror_world_updater.utils.mcdr_util import mkcmd
+
 
 class TextComponent:
     @classmethod
@@ -11,3 +13,14 @@ class TextComponent:
     @classmethod
     def title(cls, text: Any) -> RTextBase:
         return RTextList(RText('======== ', RColor.gray), text, RText(' ========', RColor.gray))
+
+    @classmethod
+    def command(cls, s: str, *, color: RColor = RColor.gray, suggest: bool = False, run: bool = False,
+                raw: bool = False) -> RTextBase:
+        cmd = s if raw else mkcmd(s)
+        text = RText(cmd, color)
+        if suggest:
+            text.h(cls.tr('command.suggest', cmd)).c(RAction.suggest_command, cmd)
+        elif run:
+            text.h(cls.tr('command.run', cmd)).c(RAction.run_command, cmd)
+        return text
