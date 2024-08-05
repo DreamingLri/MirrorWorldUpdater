201425b7da34d450c3e3a78e7f8b9c2ad1f8edba
DreamingLri <3507415455@qq.com>
2024-08-05 10:55:47 +0800

    ♻️ reform upstream

--- mirror_world_updater/config/config.py
+++ mirror_world_updater/config/config.py
@@ -4,9 +4,20 @@ from typing import Optional, List
 from mcdreforged.api.all import *
 
 
+class ServerInfo(Serializable):
+    server: str
+    server_path: str
+
+
 class Config(Serializable):
     permission: int = PermissionLevel.HELPER
+    upstream: str = 'survival'
     upstream_server_path: str = '../survival/server'
+    upstream_list: List[ServerInfo] = [
+        ServerInfo(server='survival', server_path='../survival/server'),
+        ServerInfo(server='mirror', server_path='../mirror/server'),
+        ServerInfo(server='creative', server_path='../creative/server'),
+    ]
     self_server_path: str = './server'
     world_names: List[str] = ['world']
     count_down: int = 10
