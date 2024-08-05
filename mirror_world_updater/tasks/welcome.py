6e9b55a720caa4bb66f9201e09262f5a51893148
DreamingLri <3507415455@qq.com>
2024-08-05 09:52:35 +0800

    ♻️ reform welcome message

---
+++ mirror_world_updater/tasks/welcome.py
@@ -0,0 +1,53 @@
+from abc import ABC
+from typing import Union, Dict
+
+from mcdreforged.api.all import *
+
+from mirror_world_updater import mcdr_globals
+from mirror_world_updater.tasks.help import HelpMessage
+from mirror_world_updater.tasks.task import Task
+from mirror_world_updater.text_component import TextComponent
+from mirror_world_updater.utils import help_message_utils
+from mirror_world_updater.utils.utils import mk_cmd
+
+
+class Welcome(Task, ABC):
+    COMMON_COMMANDS = ['', 'help', 'upstream', 'sync', 'confirm', 'abort']
+
+    def __init__(self, source: CommandSource):
+        super().__init__(source)
+
+    def id(self) -> str:
+        return 'welcome'
+
+    def reply(self, msg: Union[str, RTextBase], *, with_prefix: bool = False):
+        super().reply(msg, with_prefix=with_prefix)
+
+    def show_welcome(self) -> None:
+        self.reply(TextComponent.title(self.tr(
+            'title',
+            name=RText(mcdr_globals.metadata.name, RColor.dark_aqua),
+            version=RText(f'v{mcdr_globals.metadata.version}', RColor.gold),
+        )))
+        self.reply(mcdr_globals.metadata.get_description_rtext())
+
+        self.reply(
+            self.tr('common_commands').
+            set_color(RColor.light_purple).
+            h(self.tr('common_commands.hover', TextComponent.command('help'))).
+            c(RAction.suggest_command, mk_cmd('help'))
+        )
+        helps = self.__generate_command_helps()
+        for cmd in self.COMMON_COMMANDS:
+            self.reply(helps[cmd])
+
+    def __generate_command_helps(self) -> Dict[str, RTextBase]:
+        msg = HelpMessage(self.source).tr('commands.content', prefix=self.__cmd_prefix)
+        with self.source.preferred_language_context():
+            return {h.literal: h.text for h in help_message_utils.parse_help_message(msg)}
+
+    @property
+    def __cmd_prefix(self) -> str:
+        return self.config.command.prefix
+
+
