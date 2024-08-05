fa29420c309919befa10ddb9b9bb12dd9ac33af3
DreamingLri <3507415455@qq.com>
2024-08-05 10:55:31 +0800

    ♻️ reform upstream

--- mirror_world_updater/command/commands.py
+++ mirror_world_updater/command/commands.py
@@ -1,6 +1,9 @@
 from mcdreforged.api.all import *
 
 from mirror_world_updater.config.config import Config
+from mirror_world_updater.tasks.help import HelpMessage
+from mirror_world_updater.tasks.upstream import Upstream
+from mirror_world_updater.utils.utils import reply_message, tr, mk_cmd
 
 
 class CommandManager:
@@ -8,11 +11,22 @@ class CommandManager:
         self.server = server
         self.config = Config.get()
 
-    def cmd_help(self):
-        pass
+    def cmd_help(self, source: CommandSource, context: dict):
+        what = context.get('what')
+        if what is not None and what not in HelpMessage:
+            reply_message(source, tr('command.help.no_help', RText(mk_cmd(what), RColor.gray)))
+            return
 
-    def cmd_upstream(self):
-        pass
+        HelpMessage(source).show_help()
+
+    def set_upstream(self, source: CommandSource, context: CommandContext):
+        server_name = context['server']
+        if server_name is None:
+            reply_message(source, RText(tr('command.upstream.no_server'), RColor.red))
+        Upstream(source).set_upstream(server_name)
+
+    def list_upstream(self, source: CommandSource, _):
+        Upstream(source).list_upstream()
 
     def cmd_sync(self):
         pass
@@ -23,8 +37,14 @@ class CommandManager:
     def register_command(self):
         builder = SimpleCommandBuilder()
 
+        # help
         builder.command('help', self.cmd_help)
-        builder.command('upstream', self.cmd_upstream)
+        builder.command('help <what>', self.cmd_help)
+        # upstream
+        builder.command('upstream', lambda src: self.cmd_help(src, {'what': 'upstream'}))
+        builder.command('upstream set <server>', self.set_upstream)
+        builder.command('upstream list', self.list_upstream)
+        # sync
         builder.command('sync', self.cmd_sync)
 
         root = (
--- mirror_world_updater/tasks/upstream.py
+++ mirror_world_updater/tasks/upstream.py
@@ -1,6 +1,7 @@
 from abc import ABC
+from typing import Union
 
-from mcdreforged.command.command_source import CommandSource
+from mcdreforged.api.all import *
 
 from mirror_world_updater.tasks.task import Task
 
@@ -8,6 +9,37 @@ from mirror_world_updater.tasks.task import Task
 class Upstream(Task, ABC):
     def __init__(self, source: CommandSource):
         super().__init__(source)
+        self.update_config = self.config.get()
+        self.upstream = self.config.get().upstream
+        self.upstream_server_path = self.config.get().upstream_server_path
+        self.upstream_list = self.config.get().upstream_list
 
     def id(self) -> str:
         return 'upstream'
+
+    def reply(self, msg: Union[str, RTextBase], *, with_prefix: bool = False):
+        super().reply(msg, with_prefix=with_prefix)
+
+    def list_upstream(self) -> None:
+        for server_info in self.upstream_list:
+            self.reply(self.tr("title"))
+            self.reply(self.tr(
+                "current_upstream",
+                name=RText(self.upstream, RColor.dark_aqua),
+                path=RText(self.upstream_server_path, RColor.green)))
+            self.reply(self.tr(
+                "list_upstream",
+                name=RText(server_info.server, RColor.dark_aqua),
+                path=RText(server_info.server_path, RColor.green)))
+
+    def set_upstream(self, server_name: str) -> None:
+        for server_info in self.upstream_list:
+            if server_info.server == server_name:
+                self.update_config.upstream = server_name
+                self.update_config.upstream_server_path = server_info.server_path
+                self.reply(self.tr("set_success", RText(server_name, RColor.dark_aqua)))
+                self.server.save_config_simple(self.config)
+                return
+
+        self.reply(self.tr("no_upstream", RText(server_name, RColor.red)))
+        return
